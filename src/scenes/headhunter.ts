384d7e68f4151dfb63be223b7468b4b8acf81ace
Magomed Gasanov <magomedgasanov.03@bk.ru>
2022-05-22 15:45:37 +0300

    Оптимизация под поиск по специальности

--- src/scenes/headhunter.ts
+++ src/scenes/headhunter.ts
@@ -4,7 +4,7 @@ import { Scenes } from 'telegraf';
 const scene = new Scenes.WizardScene(
   'hh-scene',
   async (ctx) => {
-    await ctx.reply('Введите вашу специальность:', {
+    await ctx.reply('Введите вашу специальность, должность\n(или другие ключевые слова):', {
       reply_markup: { remove_keyboard: true },
     });
     return ctx.wizard.next();
